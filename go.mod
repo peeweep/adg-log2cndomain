437c6f56ba89d77097661a330f0539bc94a0f1b9
jinqiang zhang <peeweep@0x0.ee>
2023-11-02 03:59:34 +0000

    init
    Signed-off-by: jinqiang zhang <peeweep@0x0.ee>

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/peeweep/adg-log2cndomain
+
+go 1.21.3
+
+require (
+	github.com/metacubex/geo v0.0.0-20230810094848-a465d708c02b
+	github.com/miekg/dns v1.1.56
+)
+
+require (
+	github.com/sagernet/sing v0.2.7 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/metacubex/geo/geosite"
+	"github.com/miekg/dns"
+	"os"
+	"strings"
+)
+
+type LogEntry struct {
+	T        string `json:"T"`
+	QH       string `json:"QH"`
+	QT       string `json:"QT"`
+	QC       string `json:"QC"`
+	CP       string `json:"CP"`
+	Answer   string `json:"Answer"`
+	Result   Result `json:"Result"`
+	Upstream string `json:"Upstream"`
+	IP       string `json:"IP"`
+	Elapsed  int    `json:"Elapsed"`
+}
+
+type Result struct{}
+
+//var geoip.data geoipDb  = geoip.FromFile("/home/peeweep/.geo/geoip.dat")
+
+//var db *geoip.Database
+//db, err = geoip.FromFile(filePath)
+//fmt.Println(geoipDat.LookupCode(net.IP(ipAddr)))
+
+func main() {
+
+	geositeFilePath := "/home/peeweep/.geo/geosite.dat"
+	db, err := geosite.FromFile(geositeFilePath)
+	if err != nil {
+		fmt.Println("Error when loading", geositeFilePath, "as a GeoSite database, skipped.")
+		return
+	}
+
+	// 将JSON数据分割成多个条目
+	jsonFile := "querylog.json"
+	fileContent, err := os.ReadFile(jsonFile)
+	if err != nil {
+		fmt.Println("Error when loading", jsonFile)
+		return
+	}
+	entries := splitJSON(string(fileContent))
+
+	msg := &dns.Msg{}
+	var domains []string
+
+	// 遍历每个条目并解析
+	for i, entry := range entries {
+		var logEntry LogEntry
+		err := json.Unmarshal([]byte(entry), &logEntry)
+		if err != nil {
+			fmt.Printf("解析第 %d 条JSON时出错: %s\n", i+1, err)
+		} else {
+			// base64 decode Answer
+			decodedBytes, err := base64.StdEncoding.DecodeString(logEntry.Answer)
+			if err != nil {
+				fmt.Println("解码时出错:", err)
+				return
+			}
+
+			err = msg.Unpack([]byte(decodedBytes))
+			if err != nil {
+				fmt.Println("解析DNS消息时出错:", err)
+				return
+			}
+
+			// check question is in geosite:cn
+			newDomains := checkGeosite(msg, db, domains)
+			// update domains or skip
+			if len(newDomains) == len(domains) {
+				continue
+			} else {
+				domains = newDomains
+			}
+
+			// answer
+			for _, answer := range msg.Answer {
+
+				if answer.Header().Rrtype == dns.TypeA {
+					ipAddr := strings.TrimPrefix(
+						answer.String(),
+						answer.Header().String())
+
+					domainAnswerName := strings.TrimSuffix(answer.Header().Name, ".")
+
+					fmt.Printf("Answer: %s %s\n", domainAnswerName, ipAddr)
+				}
+			}
+
+		}
+	}
+	fmt.Println(domains)
+
+}
+
+func splitJSON(jsonData string) []string {
+	// 将JSON数据按换行符分割成多个条目
+	entries := []string{}
+	start := 0
+	for i := 0; i < len(jsonData); i++ {
+		if jsonData[i] == '\n' {
+			entries = append(entries, jsonData[start:i])
+			start = i + 1
+		}
+	}
+	// 添加最后一个条目（没有换行符）
+	if start < len(jsonData) {
+		entries = append(entries, jsonData[start:])
+	}
+	return entries
+}
+
+// is already in geosite:cn
+func isGeositeCN(db *geosite.Database, domain string) bool {
+	codes := db.LookupCodes(domain)
+	for i := range codes {
+		if codes[i] == "cn" {
+			return true
+		}
+	}
+	return false
+}
+
+// parse geosite
+func checkGeosite(msg *dns.Msg, db *geosite.Database, domains []string) []string {
+
+	// question
+	for _, question := range msg.Question {
+		if question.Qtype == dns.TypeA {
+			domainQuestionName := strings.TrimSuffix(question.Name, ".")
+			fmt.Println("check domain:  ", domainQuestionName)
+			//fmt.Printf("Question: %s %s\n", domainQuestionName, dns.Type(question.Qtype).String())
+
+			if !isGeositeCN(db, domainQuestionName) {
+				domains = append(domains, domainQuestionName)
+				fmt.Println("add domain: ", domainQuestionName)
+				return domains
+			}
+		}
+	}
+	return domains
+
+}
